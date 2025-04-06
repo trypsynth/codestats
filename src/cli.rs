20590ded21250ae622ce29b3895bea6b1a60fad3
Quin <TheQuinbox@gmail.com>
2025-04-06 10:22:43 -0600

    Clean up cli by making the help more consistent.

--- src/cli.rs
+++ src/cli.rs
@@ -4,20 +4,20 @@ use std::path::PathBuf;
 #[derive(Parser)]
 #[command(version, about)]
 pub struct Cli {
-    #[arg(help = "The path to analyze.")]
+    #[arg(help = "The path to analyze")]
     pub path: PathBuf,
-    #[arg(short, long, help = "Show much more verbose output.")]
+    #[arg(short, long, help = "Show much more verbose output")]
     pub verbose: bool,
     #[arg(
         short,
         long,
         default_value_t = true,
-        help = "Respect .gitignore/.ignore files."
+        help = "Respect .gitignore/.ignore files"
     )]
     pub ignores: bool,
-    #[arg(short, long, help = "Follow symlinks, if encountered.")]
+    #[arg(short, long, help = "Follow symlinks")]
     pub symlinks: bool,
-    #[arg(short, long, help = "Ignore hidden files.", default_value_t = true)]
+    #[arg(short, long, help = default_value_t = true, "Ignore hidden files")]
     pub no_hidden: bool,
 }
 
