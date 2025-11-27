a55ca492ce101c2b3067be27d95738dc92c9c8fe
Quin Gillespie <trypsynth@gmail.com>
2025-11-27 09:34:01 -0700

    chore: format

--- src/analysis/analyzer.rs
+++ src/analysis/analyzer.rs
@@ -188,10 +188,12 @@ impl CodeAnalyzer {
 			if bytes_read == 0 {
 				break;
 			}
-			let line_type = if let Ok(line) = str::from_utf8(&buffer) { line_classifier::classify_line(line, lang_info, &mut comment_state, is_first_line) } else {
-   					let line = String::from_utf8_lossy(&buffer);
-   					line_classifier::classify_line(line.as_ref(), lang_info, &mut comment_state, is_first_line)
-   				};
+			let line_type = if let Ok(line) = str::from_utf8(&buffer) {
+				line_classifier::classify_line(line, lang_info, &mut comment_state, is_first_line)
+			} else {
+				let line = String::from_utf8_lossy(&buffer);
+				line_classifier::classify_line(line.as_ref(), lang_info, &mut comment_state, is_first_line)
+			};
 			match line_type {
 				LineType::Code => code_lines += 1,
 				LineType::Comment => comment_lines += 1,
