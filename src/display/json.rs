c647c31fdb0e18dcba59ffe320d3026090ca1afe
Quin Gillespie <trypsynth@gmail.com>
2025-12-13 07:33:09 -0700

    Put the JSON display modules into one file.

--- src/display/json.rs
+++ src/display/json.rs
@@ -1,7 +1,7 @@
 use std::{io::Write, path::Path};
 
 use anyhow::Result;
-use serde_json::to_writer_pretty;
+use serde_json::{to_writer, to_writer_pretty};
 
 use super::{OutputFormatter, ViewOptions};
 use crate::analysis::AnalysisResults;
@@ -19,6 +19,24 @@ impl OutputFormatter for JsonFormatter {
 	) -> Result<()> {
 		let (_ctx, report) = self.prepare_report(results, path, verbose, view_options);
 		to_writer_pretty(&mut *writer, &report)?;
+		writeln!(writer)?;
+		Ok(())
+	}
+}
+
+pub struct JsonCompactFormatter;
+
+impl OutputFormatter for JsonCompactFormatter {
+	fn write_output(
+		&self,
+		results: &AnalysisResults,
+		path: &Path,
+		verbose: bool,
+		view_options: ViewOptions,
+		writer: &mut dyn Write,
+	) -> Result<()> {
+		let (_ctx, report) = self.prepare_report(results, path, verbose, view_options);
+		to_writer(&mut *writer, &report)?;
 		writeln!(writer)?;
 		Ok(())
 	}
